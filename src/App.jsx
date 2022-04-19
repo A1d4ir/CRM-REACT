483e960eefba0bfae990a368cf8bfe81981e843d
Aldair <aldair_fime@hotmail.com>
2022-04-19 15:10:38 -0500

    Aplicacion terminada

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Route path='/clientes' element={<Layout />}>
           <Route index element={<Inicio />} />
           <Route path='nuevo' element={<NuevoCliente />} />
-          <Route path='editar' element={<EditarCliente />} />
+          <Route path='editar/:id' element={<EditarCliente />} />
           <Route path=':id' element={<VerCliente />} />
         </Route>
       </Routes>
--- src/paginas/VerCliente.jsx
+++ src/paginas/VerCliente.jsx
@@ -1,16 +1,16 @@
 import { useParams } from 'react-router-dom'
 import {useEffect, useState} from 'react'
+import Spinner from '../components/Spinner'
 
 const VerCliente = () => {
 
     const [cliente, setCliente] = useState({})
-    const [cargando, setCargando] = useState(false)
+    const [cargando, setCargando] = useState(true)
 
     const { id } = useParams()
 
     useEffect(() => {
 
-        setCargando(!cargando)
         const obtenerClienteAPI = async () => {
             try {
 
@@ -22,7 +22,9 @@ const VerCliente = () => {
             } catch (error) {
                 console.log(error)
             }
-            setCargando(false)
+
+            setCargando(!cargando)
+        
         }
 
         obtenerClienteAPI()
@@ -30,50 +32,52 @@ const VerCliente = () => {
     }, [])
 
   return (
-    <div>
-        {cargando ? 'cargando...' : (
-            <>
-                <h1 className='font-black text-4xl text-blue-900'>Ver Cliente: {cliente.nombre}</h1>
-                <p className='mt-3'>Información del cliente</p>
+    
+    cargando ? <Spinner /> : Object.keys(cliente).length === 0 ? <p>No hay resultados</p> : (
 
-                {cliente.nombre && (
-                    <p className='text-4xl text-gray-600 mt-10'>
-                        <span className='text-gray-800 uppercase font-bold'>Cliente: </span>
-                        {cliente.nombre}
-                    </p>
-                )}
+        <div>
 
-                {cliente.email && (
-                    <p className='text-2xl text-gray-600 mt-4'>
-                        <span className='text-gray-800 uppercase font-bold'>Email: </span>
-                        {cliente.email}
-                    </p>
-                )}
+            <h1 className='font-black text-4xl text-blue-900'>Ver Cliente: {cliente.nombre}</h1>
+            <p className='mt-3'>Información del cliente</p>
+
+            {cliente.nombre && (
+                <p className='text-4xl text-gray-600 mt-10'>
+                    <span className='text-gray-800 uppercase font-bold'>Cliente: </span>
+                    {cliente.nombre}
+                </p>
+            )}
+
+            {cliente.email && (
+                <p className='text-2xl text-gray-600 mt-4'>
+                    <span className='text-gray-800 uppercase font-bold'>Email: </span>
+                    {cliente.email}
+                </p>
+            )}
         
-                {cliente.telefono && (
-                    <p className='text-2xl text-gray-600 mt-4'>
-                        <span className='text-gray-800 uppercase font-bold'>Teléfono: </span>
-                        {cliente.telefono}
-                    </p>
-                )}
+            {cliente.telefono && (
+                <p className='text-2xl text-gray-600 mt-4'>
+                    <span className='text-gray-800 uppercase font-bold'>Teléfono: </span>
+                    {cliente.telefono}
+                </p>
+            )}
         
-                {cliente.empresa && (
-                    <p className='text-2xl text-gray-600 mt-4'>
-                        <span className='text-gray-800 uppercase font-bold'>Empresa: </span>
-                        {cliente.empresa}
-                    </p>
-                )}
+            {cliente.empresa && (
+                <p className='text-2xl text-gray-600 mt-4'>
+                    <span className='text-gray-800 uppercase font-bold'>Empresa: </span>
+                    {cliente.empresa}
+                </p>
+            )}
         
-                {cliente.notas && (
-                    <p className='text-2xl text-gray-600 mt-4'>
-                        <span className='text-gray-800 uppercase font-bold'>Notas: </span>
-                        {cliente.notas}
-                    </p>
-                )}
-            </>
-        )}
+            {cliente.notas && (
+                <p className='text-2xl text-gray-600 mt-4'>
+                    <span className='text-gray-800 uppercase font-bold'>Notas: </span>
+                    {cliente.notas}
+                </p>
+            )}
         
-    </div>
+        </div>
+
+    )
   )
 }
 
